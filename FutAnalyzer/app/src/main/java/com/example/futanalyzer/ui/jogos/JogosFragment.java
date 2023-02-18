cd521426c1315ba7a85ebe225313d1fc7e8ab2bd
DimasEly <110694492+DimasEly@users.noreply.github.com>
2023-02-18 00:28:34 -0300

    começo da adição de jogo

--- FutAnalyzer/app/src/main/java/com/example/futanalyzer/ui/jogos/JogosFragment.java
+++ FutAnalyzer/app/src/main/java/com/example/futanalyzer/ui/jogos/JogosFragment.java
@@ -2,6 +2,7 @@ package com.example.futanalyzer.ui.jogos;
 
 import android.app.AlertDialog;
 import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -11,12 +12,31 @@ import android.widget.Button;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.DefaultItemAnimator;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.futanalyzer.InformacoesApp;
 import com.example.futanalyzer.R;
+import com.example.futanalyzer.adapter.ListaJogosAdapter;
 import com.example.futanalyzer.databinding.FragmentHomeBinding;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+import modelDominio.Jogo;
+import modelDominio.Usuario;
 
 public class JogosFragment extends Fragment {
-    Button teste;
+    FloatingActionButton fabAddJogo;
+    RecyclerView rvListadeJogo;
+    ListaJogosAdapter jogosAdapter;
+
+    ArrayList<Jogo> listaJogos;
+
+    InformacoesApp informacoesApp;
+    String msgRecebida;
 
     private FragmentHomeBinding binding;
 
@@ -27,6 +47,51 @@ public class JogosFragment extends Fragment {
 
         binding = FragmentHomeBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
+        rvListadeJogo = binding.rvListaJogos;
+        fabAddJogo = binding.fabAddPartida;
+
+        informacoesApp = (InformacoesApp) getActivity().getApplication();
+
+        fabAddJogo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent it = new Intent(getActivity(), CadastroJogoActivity.class);
+                startActivity(it);
+                getActivity().finish();
+            }
+        });
+
+        carregaListaJogo();
+
+        private void carregaListaJogo() {
+            Thread thread = new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    try {
+                        informacoesApp.out.writeObject("JogoLista");
+                        Usuario userLogado = informacoesApp.getUsuarioLogado();
+                        informacoesApp.out.writeObject(userLogado);
+                        listaJogos = (ArrayList<Jogo>) informacoesApp.in.readObject();
+
+                        getActivity().runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+                                jogosAdapter = new ListaJogosAdapter(listaJogadores, trataCliqueItem, trataCliqueLongo);
+                                rvListadeJogo.setLayoutManager(new LinearLayoutManager(informacoesApp));
+                                rvListadeJogo.setItemAnimator(new DefaultItemAnimator());
+                                rvListadeJogo.setAdapter(jogosAdapter);
+                            }
+                        });
+                    } catch (IOException ioe) {
+                        ioe.printStackTrace();
+                    } catch (ClassNotFoundException classe) {
+                        classe.printStackTrace();
+                    }
+                }
+            });
+            thread.start();
+        }
+
 
         return root;
 
---
+++ FutAnalyzer/app/src/main/java/modelDominio/Jogo.java
@@ -0,0 +1,67 @@
+package modelDominio;
+
+import java.io.Serializable;
+
+public class Jogo implements Serializable {
+    private static final long serialVersionUID = 123L;
+
+    private int id;
+    private int meuPlacar;
+    private int advPlacar;
+    private int idUsuario;
+
+    public Jogo(int id, int meuPlacar, int advPlacar, int idUsuario) {
+        this.id = id;
+        this.meuPlacar = meuPlacar;
+        this.advPlacar = advPlacar;
+        this.idUsuario = idUsuario;
+    }
+
+    public Jogo(int meuPlacar, int advPlacar, int idUsuario) {
+        this.meuPlacar = meuPlacar;
+        this.advPlacar = advPlacar;
+        this.idUsuario = idUsuario;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getMeuPlacar() {
+        return meuPlacar;
+    }
+
+    public void setMeuPlacar(int meuPlacar) {
+        this.meuPlacar = meuPlacar;
+    }
+
+    public int getAdvPlacar() {
+        return advPlacar;
+    }
+
+    public void setAdvPlacar(int advPlacar) {
+        this.advPlacar = advPlacar;
+    }
+
+    public int getIdUsuario() {
+        return idUsuario;
+    }
+
+    public void setIdUsuario(int idUsuario) {
+        this.idUsuario = idUsuario;
+    }
+
+    @Override
+    public String toString() {
+        return "Jogo{" +
+                "id=" + id +
+                ", meuPlacar=" + meuPlacar +
+                ", advPlacar=" + advPlacar +
+                ", idUsuario=" + idUsuario +
+                '}';
+    }
+}
